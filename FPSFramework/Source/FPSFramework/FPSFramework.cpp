b163c81bc5302313b79156d2ccd30fba18fd014e
Huw Weaver <huwweaver@gmail.com>
2025-04-05 09:30:08 +0100

    C++ First Person UE5 template
    going to build the FPSFramework back up from scratch using this

---
+++ FPSFramework/Source/FPSFramework/FPSFramework.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSFramework.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSFramework, "FPSFramework" );
+ 
